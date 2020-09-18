8d3cea97fd58a88825da36e0ab2c4a9f1da7bad0
Valerio <valerio.modesti@gmail.com>
2020-09-18 18:42:36 +0200

    init

---
+++ dist/index3.php
@@ -0,0 +1,30 @@
+<?php
+    include 'dischi-db.php';
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+     <head>
+         <meta charset="utf-8">
+         <title>php-ajax-dischi</title>
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js"></script>
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+         <link rel="stylesheet" href="app.css">
+     </head>
+     <body>
+         <header>
+             <div class="container ">
+                 <img src="logo.png" alt="logo" />
+             </div>
+         </header>
+
+         <div class="cds-container container">
+             <?php foreach ($database as $cd) { ?>
+                 <div class="cd">
+                     <img src="<?php echo $cd['poster']; ?>" alt="">
+                 </div>
+             <?php } ?>
+         </div>
+
+     </body>
+ </html>
